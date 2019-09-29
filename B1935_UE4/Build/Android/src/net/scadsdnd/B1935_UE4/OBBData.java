84dfe0fa251c34979cde684eedf2568c2edd40af
Alexnadr <scad.luncher@gmail.com>
2019-09-29 22:23:48 +0300

    Fixed light Artifacts

--- B1935_UE4/Build/Android/src/net/scadsdnd/B1935_UE4/OBBData.java
+++ B1935_UE4/Build/Android/src/net/scadsdnd/B1935_UE4/OBBData.java
@@ -18,8 +18,8 @@ mFileSize = fileSize;
 public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
-"1", // the version of the APK that the file was uploaded against
-83931286L // the length of the file in bytes
+"2", // the version of the APK that the file was uploaded against
+136380211L // the length of the file in bytes
 )
 };
 };
