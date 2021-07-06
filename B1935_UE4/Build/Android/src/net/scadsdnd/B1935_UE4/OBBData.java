1f9acd8cd30cddb254c26b7e5b3b58ae74339331
Alexnadr <scad.luncher@gmail.com>
2021-07-06 20:46:06 +0300

    project structure update

--- B1935_UE4/Build/Android/src/net/scadsdnd/B1935_UE4/OBBData.java
+++ B1935_UE4/Build/Android/src/net/scadsdnd/B1935_UE4/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "2", // the version of the APK that the file was uploaded against
-136380211L // the length of the file in bytes
+236682354L // the length of the file in bytes
 )
 };
 };
